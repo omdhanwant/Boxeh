16a8e6f438f040d4c4d3ddb1565b163f6a827e7f
Om Dhanwant <om.dhanwant@hotelkeyapp.com>
2020-02-01 19:14:42 +0530

    added side menu added login screen added register screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+  SideMenuComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
---
+++ src/app/side-menu/side-menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {  MenuController } from '@ionic/angular';
+import { RouterEvent, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+})
+export class SideMenuComponent {
+
+  pages = [
+    {
+      pageName: 'Home',
+      url: '/home'
+    },
+    {
+      pageName: 'Login/ Register',
+    url: '/login'
+  }];
+
+  selectedPath = '';
+
+  constructor(private menu: MenuController, private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+      console.log(this.selectedPath);
+    });
+   }
+
+  // openFirst() {
+  //   this.menu.enable(true, 'first');
+  //   this.menu.open('first');
+  // }
+
+}
