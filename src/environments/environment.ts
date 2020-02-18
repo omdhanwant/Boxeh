c8a88f392cb79cd948a96d1e2d1411314150e216
VikashWankhede <>
2020-02-18 15:56:49 +0530

    cors added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  hostUrl: 'https://yadoniaaccounts.com',
+  hostUrl: 'https://cors-anywhere.herokuapp.com/https://yadoniaaccounts.com',
   // hostUrl: 'https://boxeh.net',
   // version: 'v1'
 };
