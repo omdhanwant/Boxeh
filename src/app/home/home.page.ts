878e3193d0649911f1570c1f3b68d98aa55eafb2
Om Dhanwant <>
2020-03-06 22:24:35 +0530

    state management in home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,28 +19,37 @@ export class HomePage {
   constructor(private service: HomeService, private alertService: AlertService, public authService: AuthService) {}
 
   ionViewWillEnter() {
-    this.alertService.presentLoading('Please wait...');
+    if (!this.service.HomeDataState.value) {
+      this.alertService.presentLoading('Please wait...');
+    }
+    
   }
 
   ngOnInit() {
   }
 
   initData(event?) {
-    this.subscription = this.service.getHomeData().subscribe(home => {
-      if (home.code === 200) {
-
-        if(event) {
-          event.target.complete();
+    if (this.service.HomeDataState.value) {
+      this.homeData = this.service.HomeDataState.value;
+    } else  {
+      
+      this.subscription = this.service.getHomeData().subscribe(home => {
+        if (home.code === 200) {
+  
+          if(event) {
+            event.target.complete();
+          }
+          this.homeData = home;
+          this.segment = this.homeData.data.section_week_recipes.single_recipe_content[0].tab_pane
+          this.recipeSegment = this.homeData.data.section_week_recipes.single_recipe_content[0].tablist[0][0].id
+          this.alertService.dismissLoading();
+        } else {
+          this.alertService.presentAlert(Utils.ERROR, home.message, [Utils.OK]);
+          this.alertService.dismissLoading();
         }
-        this.homeData = home;
-        this.segment = this.homeData.data.section_week_recipes.single_recipe_content[0].tab_pane
-        this.recipeSegment = this.homeData.data.section_week_recipes.single_recipe_content[0].tablist[0][0].id
-        this.alertService.dismissLoading();
-      } else {
-        this.alertService.presentAlert(Utils.ERROR, home.message, [Utils.OK]);
-        this.alertService.dismissLoading();
-      }
-    });
+      });
+    }
+
   }
 
   ionViewDidEnter() {
@@ -48,6 +57,7 @@ export class HomePage {
   }
 
   refresh(event) {
+    this.service.HomeDataState.next(null); // refresh state
     this.alertService.presentLoading('Please wait...');
     this.initData(event);
   }
@@ -55,7 +65,7 @@ export class HomePage {
   
  
   segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
+    // console.log('Segment changed', ev);
    const index =  this.homeData.data.section_week_recipes.weeks_to_show.findIndex(weeks => weeks.name === ev.target.innerText);
     // this.segment = this.homeData.data.section_week_recipes.single_recipe_content[0].tab_pane
     if(index !== -1) {
@@ -65,7 +75,7 @@ export class HomePage {
   }
 
   recipeSegmentChanged(event) {
-    console.log('Segment changed', event);
+    // console.log('Segment changed', event);
     
   }
 }
--- src/app/home/service/home.service.ts
+++ src/app/home/service/home.service.ts
@@ -2,19 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { User } from 'src/app/shared-module/models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
-
+  private homeDataState: BehaviorSubject<Home> = new BehaviorSubject(null);
   constructor(private http: HttpClient) { }
+
+  get HomeDataState() {
+    return this.homeDataState;
+  }
 
   getHomeData() {
     const time = new Date();
     return this.http.get(`${environment.hostUrl}/boxeh/apis/page-home.php?type=` + time.getTime())
       .pipe(
         map((response: Home) => {
+          this.homeDataState.next(response);
           return response;
         }));
   }
