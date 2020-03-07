1ce07fd8e097c18144c583a2201b398b4378142f
VikasWankhede <vikas.wankhede@hotelkeyapp.com>
2020-03-07 23:19:28 +0530

    contact page

--- src/app/details/contact-us/contact-us.page.ts
+++ src/app/details/contact-us/contact-us.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared-module/shared-services/auth.service';
+import { NavController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+import { AlertService } from '../../shared-module/shared-services/alert-service';
+import { Utils } from '../../shared-module/utils/constants';
+import { Error } from '../../shared-module/models/Error';
+import { LoginResponse } from '../../shared-module/models/LoginResponse';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +15,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private nav: NavController,
+    private route: ActivatedRoute,
+    public alertService: AlertService) {
+
+  }
 
   ngOnInit() {
   }
+
+  contactForm(form: NgForm) {
+    console.log(form);
+    // this.alertService.presentLoading('Please Wait...');
+    // if (form.valid) {
+
+    //   this.auth.login(form.value).subscribe((response: LoginResponse) => {
+    //     if (response.code === 200) {
+    //       form.reset();
+    //       this.alertService.dismissLoading();
+    //       this.alertService.presentAlert(Utils.SUCCESS, response.message, [Utils.OK]);
+    //       const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/'
+    //       this.nav.navigateRoot([returnUrl]);
+    //     } else {
+    //       this.alertService.dismissLoading();
+    //       this.alertService.presentAlert(Utils.ERROR, response.message, [Utils.OK]);
+    //     }
+    //   }
+    //   );
+
+    // } else {
+    //   this.alertService.dismissLoading();
+    //   this.alertService.presentAlert(Utils.ERROR, 'Enter valid username and password!', [Utils.OK]);
+    // }
+  }
+
 
 }
