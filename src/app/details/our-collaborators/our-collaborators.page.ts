2ccef00c3378b72681fbbf38ab8fbaecf39f7094
VikasWankhede <vikas.wankhede@hotelkeyapp.com>
2020-03-09 10:27:49 +0530

    collaborators added

--- src/app/details/our-collaborators/our-collaborators.page.ts
+++ src/app/details/our-collaborators/our-collaborators.page.ts
@@ -40,7 +40,7 @@ export class OurCollaboratorsPage implements OnInit {
 
   refresh(event) {
     this.alertService.presentLoading('Please wait...');
-    this.subs = this.service.getOurCollaborators().subscribe(OurCollaboratorsResponse => {
+    this.subs = this.service.getCollborators().subscribe(OurCollaboratorsResponse => {
       if (OurCollaboratorsResponse.code === 200) {
         event.target.complete();
         this.OurCollaborators = OurCollaboratorsResponse;
@@ -56,5 +56,4 @@ export class OurCollaboratorsPage implements OnInit {
   ionViewWillLeave() {
     this.subs.unsubscribe();
   }
-
 }
