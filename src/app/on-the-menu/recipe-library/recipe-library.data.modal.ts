95958d8eef38fdac520bc621c905293997f9542b
VikasWankhede <vikas.wankhede@hotelkeyapp.com>
2020-03-21 00:06:55 +0530

    recipe page added

---
+++ src/app/on-the-menu/recipe-library/recipe-library.data.modal.ts
@@ -0,0 +1,39 @@
+interface RecipeLibrary {
+    code: number;
+    status: boolean;
+    message: string;
+    data: {
+        page_head: {
+            bg_cover:string;
+            title: string;
+            sub_title:string;
+        },
+        secion_recipes: {
+            recipes_list: [
+                {
+                    id:number;
+                    title:string;
+                    thumbnail_url:string;
+                    content:string;
+                    categories: [
+                        {
+                            id:number;
+                            name: string;
+                        },
+                        {
+                            id:number;
+                            name: string;
+                        }
+                    ],
+                    weeks:string;
+                    recipe_cooking_time: string;
+                    recipe_extra_info: string;
+                }
+            ]
+        },
+        section_footer: {
+            button_url: string;
+            text: string;
+        }
+    }
+}
--- src/app/on-the-menu/recipe-library/recipe-library.page.ts
+++ src/app/on-the-menu/recipe-library/recipe-library.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from './service.service';
+import { AlertService } from 'src/app/shared-module/shared-services/alert-service';
+import { Utils } from 'src/app/shared-module/utils/constants';
+import { Subscription } from 'rxjs';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+import { AuthService } from '../../shared-module/shared-services/auth.service';
 
 @Component({
   selector: 'app-recipe-library',
@@ -6,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-library.page.scss'],
 })
 export class RecipeLibraryPage implements OnInit {
-
-  constructor() { }
+  RecipeLibrary: RecipeLibrary = null;
+  subscription: Subscription;
+  langSubscription: Subscription;
+  constructor(private service: Service, private authService: AuthService, private alertService: AlertService, private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
+  getBackground(image) {
+      return this._sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(29, 29, 29, 0), rgba(16, 16, 23, 0.5)), url(${image})`);
+  }
+  
+  initData(event?) {
+    this.langSubscription =  this.authService.$currentLanguage.subscribe(languageState => {
+
+      // if (this.authService.LANGUAGE !== languageState) {
+      //   this.alertService.presentLoading('Please wait...');
+      //   this.service.refreshState();
+      // } 
+      if (this.service.currentPageLanguage !== languageState) {
+        this.service.currentPageLanguage = languageState
+        this.alertService.presentLoading('Please wait...');
+        this.service.refreshState();
+  
+      } 
+
+      if (this.service.RecipeLibraryDataState) {
+        this.RecipeLibrary = this.service.RecipeLibraryDataState;
+      } else  {
+        this.subscription = this.service.getRecipeLibrary(languageState).subscribe(responseData => {
+          if (responseData.code === 200) {
+    
+            if(event) {
+              event.target.complete();
+            }
+            this.RecipeLibrary = responseData;
+            this.dismissLoader();
+          } else {
+            this.alertService.presentAlert(Utils.ERROR, responseData.message, [Utils.OK]);
+            this.dismissLoader();
+          }
+        });
+      }
+
+    })
+  }
+
+  dismissLoader() {
+    setTimeout(() => {
+      this.alertService.dismissLoading();
+    }, 100); 
+  }
+
+  ionViewDidEnter() {
+    this.initData();
+  }
+
+
+  refresh(event) {
+    this.service.refreshState(); // refresh state
+    this.alertService.presentLoading('Please wait...');
+    this.initData(event);
+  }
+  
+  ionViewDidLeave(){
+    if (this.langSubscription) this.langSubscription.unsubscribe();
+    if (this.subscription) this.subscription.unsubscribe();
+   }
 }
