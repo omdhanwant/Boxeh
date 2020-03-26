208352260075916fc713e9accb485a49ebab1dd4
Om Dhanwant <>
2020-03-26 18:11:23 +0530

    -- cart total calculations -- remove items from cart

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -6,30 +6,32 @@ import { Cart } from '../boxeh-plans/model/cart';
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
 })
-export class CartPage implements OnInit {
+export class CartPage {
   cartData: Cart[];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
   constructor() { 
     this.cartData = null;
-  }
-
-  ngOnInit() {
-  }
-
-
-  removeFromCart(index) {
-
-  }
-
-  calculateTotalCartValues() {
-
   }
 
 
   ionViewDidEnter() {
     if(localStorage.getItem('cart')) {
      this.cartData = JSON.parse(localStorage.getItem('cart'));
-      
+     this.calculateTotalCartValues();
     }
-   
+  }
+
+  // remove items from cart
+  removeFromCart(index) {
+    this.cartData.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(this.cartData));
+    this.calculateTotalCartValues();
+  }
+
+  // cart total calculations
+  calculateTotalCartValues() { 
+   this.totalPrice =  this.cartData.reduce((a, b) => a + (+b.price || 0), 0);
+   this.totalQuantity = this.cartData.reduce((a, b) => a + (+b.quantity || 0), 0);
   }
 }
