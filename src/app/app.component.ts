82ad15475165333dfda991de58218ca948bc82f4
Om Dhanwant <>
2020-04-11 00:43:54 +0530

    --hardware back button issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Router } from '@angular/router';
+import { Platform, NavController } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { Router, NavigationStart } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   isLoggedIn = false;
+  counter = 0;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private router: Router
+    private router: Router,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -27,11 +30,39 @@ export class AppComponent {
       this.splashScreen.hide();
     });
 
-    this.platform.backButton.subscribeWithPriority(9999, () => {
-      if (this.router.url === '/home') {
-        // Back button controls when user is not in Login Page
-        navigator['app'].exitApp();
-      }
-    });
+    this.listenOnHardwareBackButton();
+
+    // this.platform.backButton.subscribe(() => {
+    //   if (this.router.url === '/home') {
+    //     // Back button controls when user is not in Login Page
+    //     navigator['app'].exitApp();
+    //   }
+    // });
+  }
+
+  private listenOnHardwareBackButton() {
+    if (this.platform.is("android")) {
+      this.platform.backButton.subscribeWithPriority(0, () => {
+        if (this.router.url === "/home") {
+          // Back button controls when user is not in Login Page
+          navigator["app"].exitApp();
+        }
+
+        if (this.counter === 0) {
+          const appStr = "app";
+          navigator[appStr].exitApp();
+        }
+        this.navCtrl.back();
+      });
+      this.router.events
+        .pipe(filter((event) => event instanceof NavigationStart))
+        .subscribe((event: NavigationStart) => {
+          if (event.navigationTrigger === "popstate") {
+            this.counter--;
+          } else {
+            this.counter++;
+          }
+        });
+    }
   }
 }
